91b29878c3a6fbe6be355b06f857e60a3cd8eef9
siddarthvader-nathan <111070282+siddarthvader-nathan@users.noreply.github.com>
2024-01-19 15:35:48 -0600

    Update with correct guess from reciprocal lattice setting

--- scripts/get_kpts.py
+++ scripts/get_kpts.py
@@ -2,20 +2,23 @@ import ase.io
 import numpy as np
 
 def get_kpts_array(atoms,desired_kpra):
-"""atoms: ASE atoms object
-   kpra: K-points per reciprocal atom"""
+"""atoms: ASE atoms object, desired_kpra(int): K-points per reciprocal atom"""
+
+"""Get real space lattice vectors"""
     a1 = atoms.cell[0]
     a2 = atoms.cell[1]
     a3 = atoms.cell[2]
 
+"""Calculate reciprocal lattice vectros"""
     b1 = 2 * np.pi* (np.cross(a2,a3))/ np.dot(a1,np.cross(a2,a3))
     b2 = 2 * np.pi* (np.cross(a3,a1))/ np.dot(a2,np.cross(a3,a1))
     b3 = 2 * np.pi* (np.cross(a1,a2))/ np.dot(a3,np.cross(a1,a2))
 
-    kpts_raw = np.array([np.linalg.norm(b1,2),np.linalg.norm(b2,2),np.linalg.norm(b3,2)])
+"""Initial guess"""
+    kpts_raw = 10*np.array([np.linalg.norm(b1,2),np.linalg.norm(b2,2),np.linalg.norm(b3,2)])
     kpts = np.round(kpts_raw,0)
 
-
+"""Scaling for appropriate k-point density"""
     kpra_less = True
     kpra_more = True
 
